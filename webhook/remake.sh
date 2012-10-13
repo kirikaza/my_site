d503eceb7affdbc7ba5410c84e650b6490347044
Kirill Kazakov <k@kirikaza.ru>
2012-10-14 01:52:40 +0400

    #- Improve the webhook script: cwd, 500, log
     * Fix changing working dir when the script is run from outside.
     * Answer "500 Server Internal Error" if something fails.
     * Log all output and every "403 Forbidden" case.

--- webhook/remake.sh
+++ webhook/remake.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 PORT=39832
-KEY=`dirname $0`/key
-
 DIR="`dirname $0`"
 
 listen() {
@@ -20,12 +18,24 @@ process() {
 	read method request x
 	sed -n '/^\r$/q'
 	if [ "$method" = "POST" -a "$request" = "/`key`" ] ; then
-		echo 'HTTP/1.1 200 OK'
+		out=$(
+			{
+				cd "$DIR/.." &&
+				git pull &&
+				make clean generate
+			} 2>&1
+		)
+		if [ $? -eq 0 ] ; then
+			echo 'HTTP/1.1 200 OK'
+		else
+			echo 'HTTP/1.1 500 Internal Server Error'
+			echo >&2 "$out" # log
+		fi
 		echo
-		git pull &&
-		make -C "$DIR/.." clean generate
+		echo "$out"
 	else
 		echo 'HTTP/1.1 403 Forbidden'
+		echo >&2 "Forbidden the request $request" # log
 	fi |
 	sed 's/$/\r/'
 }
