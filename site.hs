539ad09573c96e018207464860e665f5ba0d94ba
Kirill Kazakov <k@kirikaza.ru>
2018-07-05 00:23:25 +0400

    #16 Remove "about.md", add Liberation fonts, new CSS, add GoogleAnalytics and GoSquared

--- site.hs
+++ site.hs
@@ -5,28 +5,26 @@ import Data.Monoid ((<>))
 import Hakyll
 
 --------------------------------------------------------------------------------
+
+googleAnalyticsID = "UA-34888740-1"
+goSquaredID = "GSN-366667-M"
+
 main :: IO ()
 main = hakyll $ do
-    match "images/*" $ do
+    match "styles/*.woff" $ do
         route idRoute
         compile copyFileCompiler
 
-    match "css/*" $ do
+    match "styles/*.css" $ do
         route idRoute
         compile compressCssCompiler
-
-    match "about.md" $ do
-        route $ setExtension "html"
-        let ctx = boolField "aboutPage" (const True)
-               <> defaultContext 
-        compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/default.html" ctx
-            >>= relativizeUrls
 
     match "posts/*" $ do
         route $ setExtension "html"
         let ctx = formatDate
                <> boolField "singlePostPage" (const True)
+               <> constField "googleAnalyticsID" googleAnalyticsID
+               <> constField "goSquaredID" goSquaredID
                <> defaultContext 
         compile $ pandocCompiler
             >>= loadAndApplyTemplate "templates/post.html" ctx
@@ -40,6 +38,8 @@ main = hakyll $ do
             let ctx = listField "posts" (formatDate <> defaultContext) (return posts)
                    <> constField "title" "Мои статьи"
                    <> boolField "postsListPage" (const True)
+                   <> constField "googleAnalyticsID" googleAnalyticsID
+                   <> constField "goSquaredID" goSquaredID
                    <> defaultContext
             getResourceBody
                 >>= applyAsTemplate ctx
